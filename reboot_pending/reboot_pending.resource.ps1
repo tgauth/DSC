49236f0ea29bcb8c5144686fdd7c3368d0ff90e3
Tess Gauthier <tessgauthier@microsoft.com>
2025-07-28 11:10:39 -0400

    fix test

--- reboot_pending/reboot_pending.resource.ps1
+++ reboot_pending/reboot_pending.resource.ps1
@@ -2,17 +2,17 @@
 # Licensed under the MIT License.
 
 # Reg keys are documented here: https://learn.microsoft.com/en-us/mem/configmgr/core/servers/deploy/install/list-of-prerequisite-checks#pending-system-restart
-$reasons = [System.Collections.Generic.List[string[]]]::new()
-if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { 
+$reasons = [System.Collections.Generic.List[string]]::new()
+if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) {
   $reasons.Add("Component Based Servicing")
 }
-if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { 
+if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) {
   $reasons.Add("Windows Update")
 }
-if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { 
+if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) {
   $reasons.Add("Pending File Rename Operations")
 }
-try { 
+try {
   $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
   $status = $util.DetermineIfRebootPending()
   if(($null -ne $status) -and $status.RebootPending){
